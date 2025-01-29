a45acaf9a367327ce5403b26c118656258138df9
Nick Terrell <nickrterrell@gmail.com>
2025-01-28 20:58:27 -0500

    Configuration

--- docusaurus.config.ts
+++ docusaurus.config.ts
@@ -17,8 +17,11 @@ const config: Config = {
 
   // GitHub pages deployment config.
   // If you aren't using GitHub pages, you don't need these.
-  organizationName: 'facebook', // Usually your GitHub org/user name.
-  projectName: 'docusaurus', // Usually your repo name.
+  organizationName: 'terrelln',
+  projectName: 'terrelln.github.io',
+  deploymentBranch: 'master',
+
+  trailingSlash: true,
 
   onBrokenLinks: 'throw',
   onBrokenMarkdownLinks: 'warn',
